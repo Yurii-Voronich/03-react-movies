3628527a957fee84d02489cc423e878ed4cbcb21
Yurii <voronich97@gmail.com>
2025-08-12 12:45:06 +0300

    search logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,26 @@
+import toast, { Toaster } from "react-hot-toast";
+import fetchMovies from "../../services/movieService";
+import SearchBar from "../SearchBar/SearchBar";
 import css from "./App.module.css";
+import { useState } from "react";
+import type { Movie } from "../../types/movie";
 
 const App = () => {
-  return <div className={css.app}>App</div>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const onSubmit = async (topic: string) => {
+    setMovies([]);
+    const moviesData = await fetchMovies(topic);
+    if (moviesData.length === 0) {
+      toast.error("No movies found for your request.");
+    }
+    setMovies(moviesData);
+  };
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={onSubmit} />
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
